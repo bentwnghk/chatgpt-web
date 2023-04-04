342e84aabd7a44b6db8b6bd63b1618bb8b582c61
Ben Ng <101241940+bentwnghk@users.noreply.github.com>
2023-04-04 15:28:52 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/bentwnghk/chatgpt-web/main/src/assets/avatar.jpg',
+      name: 'Mr. Ng',
+      description: '<a href="https://mr5drive.com" class="text-blue-500" target="_blank" >mr5️⃣drive.com</a>',
     },
   }
 }
