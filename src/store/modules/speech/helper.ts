90c50e35cd12c2acc9cf9a5d7df36e7a80eec456
Ben Ng <101241940+bentwnghk@users.noreply.github.com>
2023-04-18 00:48:24 +0800

    Update helper.ts

--- src/store/modules/speech/helper.ts
+++ src/store/modules/speech/helper.ts
@@ -25,7 +25,7 @@ export function defaultState(): SpeechConfig {
     showTip: true,
     autoSpeak: false,
     speechSetting: {
-      lang: 'en-US',
+      lang: 'zh-HK',
       pitch: 1,
       rate: 1,
       useDefault: false,
